8e5332877ae70d2fc23c6e405b6a302239af7b3e
bohdan <shovkopliasbohdan@gmail.com>
2024-10-29 17:12:37 +0200

    styles assingment with vue

--- CdnVue/app.js
+++ CdnVue/app.js
@@ -1,14 +1,45 @@
 Vue.createApp({
-   data(){
+   data() {
       return {
          todos: [],
-         inputVal:""
+         inputVal: "",
+         count: 0,
+         activeTodo: -1
+      };
+   },
+
+   computed: {
+      todosLength() {
+         return this.todos.length;
       }
    },
+
+
+   watch: {
+      todos(val){
+         console.log(val)
+      }
+   },
+
    methods: {
-      addTodo(){
-         if (this.inputVal) this.todos.push(this.inputVal);
+      addTodo() {
+         if (this.inputVal) this.todos = [...this.todos, this.inputVal]
          this.inputVal = "";
+      },
+      increment() {
+         this.count += 1;
+      },
+      decrement() {
+         this.count -= 1;
+      },
+      addNumber(num) {
+         return () => {
+            this.count += num;
+         };
+      },
+
+      toggle(i){
+         this.activeTodo = i
       }
-   },
-}).mount("#root")
+   }
+}).mount("#root");
